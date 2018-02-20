b54f9191c178fd844a2822b339475a6366c9d5fa
serjt12 <jandreus87@hotmail.com>
2018-02-20 17:01:59 -0500

    some basic info

--- geckos-react/src/components/MovieDisplay.js
+++ geckos-react/src/components/MovieDisplay.js
@@ -6,16 +6,19 @@ import Moviesdb from './Moviesdb';
 
 class MovieDisplay extends Component{
 renderMovieInfo(){
-  return _.map(Moviesdb, ({ Title, Year,Poster,Rated,Plot }) => {
+  return _.map(Moviesdb, ({ Title, Year,Poster,Rated,Plot, Director,Writers }) => {
     return (
-      <div key={Title} className="container">
-      <h1>{Title}</h1>
-      <img alt="Poster" src={Poster}></img>
-      <h4>{Plot}</h4>
-      <p>{Year}</p>
-      <p>{Rated}</p>
-        
+      <div class="ui segment container">
+  <img class="ui small left floated circular  image" src={Poster}></img>
+  <h1>{Title}</h1>
+  <p>{Plot}</p>
+  <p>Year: {Year}</p>
+  <p>Director: {Director}</p>
+  {/* how to render the writers one by one */}
+  <p>Writers: {Writers}</p>
+  
 
+  
       </div>
     );
   });
