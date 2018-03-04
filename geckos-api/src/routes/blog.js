363b66192903ee87fb81dbed2a723938f0341db8
Joe Moran <joe.moran2@aol.com>
2018-03-04 17:00:21 -0500

    updated show and remove in the blog controller

--- geckos-api/src/routes/blog.js
+++ geckos-api/src/routes/blog.js
@@ -2,13 +2,13 @@ import express from 'express';
 import Blog from '../models/Blog';
 
 import * as blogController from '../controllers/blogController';
-//import Blog from '../models/Blog';
+
 
 const router = express.Router();
 
 router.post('/', blogController.store);
 router.get('/', blogController.index);
-router.put('/news/:id', blogController.show);
-router.delete('/news/:id', blogController.remove);
+router.put('/:id', blogController.show);
+router.delete('/:id', blogController.remove);
 
 export default router;
